ed8bc164d97b66342331b27d19b97f966080f7d6
Clement Carre <clement.carre@bionomeex.com>
2021-10-03 12:29:02 +0200

    Remove Debug log

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -216,12 +216,11 @@ namespace Unstable
             cardIns.Init(card);
             _cards.Add(cardIns);
 
-            var cardSize = ((RectTransform)_cardPrefab.transform).sizeDelta.x / 2f;
+            var cardSize = ((RectTransform)_cardPrefab.transform).sizeDelta.x / 1.5f;
             var half = _cards.Count / 2f;
             for (int i = 0; i < _cards.Count; i++)
             {
-                Debug.Log(Vector3.right * (i - half) * cardSize + Vector3.right * (cardSize / 2f));
-                _cards[i].SetTarget(Vector3.right * (i - half) * cardSize + Vector3.right * (cardSize / 2f));
+                _cards[i].SetTarget(Vector3.right * (i - half) * cardSize + Vector3.right * (cardSize / 1.5f));
             }
         }
     }
