ff32a60f20a7424ac6af5350fe1983148d7bd3ef
Jean Baptiste Carluer <carluer.jean-baptiste@orange.fr>
2021-10-03 11:44:32 +0200

    Add menu that redirect to game

---
+++ Assets/Scripts/UI/Menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public Button StartButton;
+    public Button OptionButton;
+    void Start(){
+        StartButton.onClick.AddListener(PlayIsClicked);
+        OptionButton.onClick.AddListener(OptionIsClicked);
+    }
+     
+    // Update is called once per frame
+    void Update () {
+    
+    }
+
+    public void PlayIsClicked()
+    {
+        SceneManager.LoadScene("Main");
+    }
+
+    public void OptionIsClicked()
+    {
+        SceneManager.LoadScene("Option");
+    }
+
+    public void ExitIsClicked()
+    {
+        
+    }
+}
