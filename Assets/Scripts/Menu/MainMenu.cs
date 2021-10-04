61592134c30c64abb24c83d5670d960837a86249
Xwilarg <xwilarg@yahoo.fr>
2021-10-04 18:17:41 +0200

    Fix menu not remembering

--- Assets/Scripts/Menu/MainMenu.cs
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -34,7 +34,6 @@ namespace Unstable.Menu
 
         private void Start()
         {
-            UpdateCardsCount();
             foreach (var c in _checkboxs)
             {
                 c.CountInfo.text = "Cards count: " + GlobalData.Instance.GetCardsCount(c.Name);
@@ -45,7 +44,24 @@ namespace Unstable.Menu
                 l.Sprite.gameObject.SetActive(GlobalData.Instance.EndingsData.Contains(l.Trigram));
             }
 
+            foreach (var l in GlobalData.Instance.DecksAllowed)
+            {
+                var c = _checkboxs.FirstOrDefault(x => x.Name.ToUpperInvariant() == l.ToUpperInvariant())
+                c.Checkbox.sprite = _checked;
+            }
+
+            if (GlobalData.Instance.SkipTutorial)
+            {
+                _checkboxTutorial.sprite = _checked;
+            }
+
+            if (GlobalData.Instance.MuteAudio)
+            {
+                _checkboxAudio.sprite = _checked;
+            }
+
             _bestScore.text = "Best Score: " + GlobalData.Instance.BestScore.ToString();
+            UpdateCardsCount();
         }
 
         public void UpdateCardsCount()
@@ -64,12 +80,14 @@ namespace Unstable.Menu
         {
             GlobalData.Instance.MuteAudio = !GlobalData.Instance.MuteAudio;
             _checkboxAudio.sprite = GlobalData.Instance.MuteAudio ? _checked : _notChecked;
+            GlobalData.Instance.Save();
         }
 
         public void SkipTutorial()
         {
             GlobalData.Instance.SkipTutorial = !GlobalData.Instance.SkipTutorial;
             _checkboxTutorial.sprite = GlobalData.Instance.SkipTutorial ? _checked : _notChecked;
+            GlobalData.Instance.Save();
         }
     }
 }
