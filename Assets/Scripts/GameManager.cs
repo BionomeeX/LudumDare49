9e72dc3ababa9e3b217c287bd6129215c4d9a60a
Xwilarg <xwilarg@yahoo.fr>
2021-10-04 13:20:45 +0200

    Fix impossible choice

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -422,7 +422,13 @@ namespace Unstable
                             Name = "New personnel available",
                             Description = "New personnel is available, select which one you want to add to your team.",
                             IsCrisis = false,
-                            Choices = new EventChoice[] { choice1, choice2 }
+                            Choices = new EventChoice[] { choice1, choice2,
+                                new EventChoice()
+                                {
+                                    Cost = "NONE",
+                                    Description = "We don't need anyone else for now"
+                                }
+                            }
                         }
                     );
                 }
