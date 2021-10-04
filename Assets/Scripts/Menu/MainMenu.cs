3d227446288701ad66f692512c3901945c0e8d59
Xwilarg <xwilarg@yahoo.fr>
2021-10-04 11:44:29 +0200

    Improve main menu

---
+++ Assets/Scripts/Menu/MainMenu.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace Unstable.Menu
+{
+    public class MainMenu : MonoBehaviour
+    {
+        public void Play()
+        {
+            SceneManager.LoadScene("Main");
+        }
+    }
+}
